0a2702dad07e88515aeec1c0d95bcf07d03f34a6
solufime <soporte.solufime@gmail.com>
2023-08-03 13:40:44 -0400

    fix: Most common errors with operations have been handled.

--- app.js
+++ app.js
@@ -1,39 +1,38 @@
 var add = function (num1, num2) { return num1 + num2; };
 var subt = function (num1, num2) { return num1 - num2; };
 var mult = function (num1, num2) { return num1 * num2; };
-var div = function (num1, num2) { return (num2 !== 0) ? num1 / num2 : "Can't divide by zero"; };
+var divd = function (num1, num2) { return (num2 !== 0) ? num1 / num2 : "Can't divide by zero"; };
 var pot = function (num) { return num * num; };
 var sqrt = function (num) { return Math.sqrt(num); };
 function calc(callback, num1, num2) {
-    console.log(callback);
-    if (!num1) {
-        return "Missed first Param";
+    //console.log(num1);
+    if (typeof num1 == "undefined" || num1 === null) {
+        return "The first parameter is missing";
     }
     if (typeof num1 !== 'number') {
-        return "Only numbers are accepted.";
+        return "Only numbers accepted.";
     }
-    if (num2) {
+    if (!num2) {
+        console.log(callback.name);
+        if (callback.name !== "sqrt" && callback.name !== "pot") {
+            return "This function required two numeric parameters, and you have passed only one";
+        }
+        return callback(num1);
+        //return;
+    }
+    else {
         if (typeof num2 !== 'number') {
-            return "Only numbers are accepted.";
+            return "Only numbers accepted";
         }
         return callback(num1, num2);
     }
-    else {
-        return callback(num1);
-    }
 }
 ;
-/*
-console.log(sqrt(25));
-console.log(div(25, 0));
-console.log(div(25, 10));
-console.log(add(25, 10));
-console.log(subt(25, 10));
-*/
 //console.log(calc(add, 25, 10));
 //console.log(calc(add, 25));
-console.log(calc(subt, 25, 5));
-console.log(calc(subt, 25, 10));
+//console.log(calc(subt, 25, 5));
 //console.log(calc(subt, 25, 10));
-//console.log(calc(div, 25, 10));
-//console.log(calc(sqrt, 25)); // error
+//console.log(calc(subt, 25, 10));
+//console.log(calc(divd, 25, 10));
+//console.log(calc(sqrt, 25));
+console.log(calc(pot, 9));
--- app.ts
+++ app.ts
@@ -1,30 +1,38 @@
 const add = (num1:number, num2:number):number => num1 + num2;
 const subt = (num1:number, num2:number):number  => num1 - num2;
 const mult = (num1:number, num2:number):number  => num1 * num2;
-const div = (num1:number, num2:number):number | string => (num2 !== 0) ? num1 / num2 : "Can't divide by zero"; 
+const divd = (num1:number, num2:number):number | string => (num2 !== 0) ? num1 / num2 : "Can't divide by zero"; 
 const pot = (num:number):number => num * num;
 const sqrt = (num:number):number => Math.sqrt( num );
 
 function calc(callback:any, num1:number, num2?:number):number | string {
-	console.log(callback)
-	if( !num1 ){ return "Missed first Param" }
-	if( typeof num1 !== 'number') {  return "Only numbers are accepted." }
-	if(num2){
-		if( typeof num2 !== 'number') {  return "Only numbers are accepted." }
-		return callback(num1, num2)
-	} else { return callback(num1) }
+
+    //console.log(num1);
+    if (typeof num1 == "undefined" || num1 === null) {
+        return "The first parameter is missing";
+    }
+    if (typeof num1 !== 'number') {
+        return "Only numbers accepted.";
+    }
+    if (!num2) {
+		console.log(callback.name)
+		if( callback.name !== "sqrt" && callback.name !== "pot" ) {
+			return "This function required two numeric parameters, and you have passed only one";
+		}
+        return callback(num1);
+        //return;
+    }
+    else {
+        if (typeof num2 !== 'number') { return "Only numbers accepted"; }
+        return callback(num1, num2);
+    }
 };
-/*
-console.log(sqrt(25));
-console.log(div(25, 0));
-console.log(div(25, 10));
-console.log(add(25, 10));
-console.log(subt(25, 10));
-*/
+
 //console.log(calc(add, 25, 10));
 //console.log(calc(add, 25));
-console.log(calc(subt, 25, 5));
-console.log(calc(subt, 25, 10));
+//console.log(calc(subt, 25, 5));
 //console.log(calc(subt, 25, 10));
-//console.log(calc(div, 25, 10));
-//console.log(calc(sqrt, 25)); // error
+//console.log(calc(subt, 25, 10));
+//console.log(calc(divd, 25, 10));
+//console.log(calc(sqrt, 25));
+console.log(calc(pot, 9));
