d9252c2e5f7b47d1092288691c0ddc3ec0bbba1b
ibernabel <id.bernabel@gmail.com>
2023-08-02 23:25:04 -0400

    refactor: working on features

--- app.js
+++ app.js
@@ -2,14 +2,25 @@ var add = function (num1, num2) { return num1 + num2; };
 var subt = function (num1, num2) { return num1 - num2; };
 var mult = function (num1, num2) { return num1 * num2; };
 var div = function (num1, num2) { return (num2 !== 0) ? num1 / num2 : "Can't divide by zero"; };
-var pot = function (num1, num2) { return num1 * num1; };
-var sqrt = function (num1, num2) { return Math.sqrt(num1); };
+var pot = function (num) { return num * num; };
+var sqrt = function (num) { return Math.sqrt(num); };
 function calc(callback, num1, num2) {
-    typeof num1 !== 'number' ? "Only numbers are accepted." : "";
-    if (num2) {
-        typeof num2 !== 'number' ? "Only numbers are accepted." : "";
+    console.log(callback);
+    if (!num1) {
+        return "Missed first Param";
     }
-    return callback(num1, num2);
+    if (typeof num1 !== 'number') {
+        return "Only numbers are accepted.";
+    }
+    if (num2) {
+        if (typeof num2 !== 'number') {
+            return "Only numbers are accepted.";
+        }
+        return callback(num1, num2);
+    }
+    else {
+        return callback(num1);
+    }
 }
 ;
 /*
@@ -20,7 +31,9 @@ console.log(add(25, 10));
 console.log(subt(25, 10));
 */
 //console.log(calc(add, 25, 10));
-console.log(calc(add, 25));
+//console.log(calc(add, 25));
+console.log(calc(subt, 25, 5));
+console.log(calc(subt, 25, 10));
 //console.log(calc(subt, 25, 10));
 //console.log(calc(div, 25, 10));
 //console.log(calc(sqrt, 25)); // error
--- app.ts
+++ app.ts
@@ -2,17 +2,17 @@ const add = (num1:number, num2:number):number => num1 + num2;
 const subt = (num1:number, num2:number):number  => num1 - num2;
 const mult = (num1:number, num2:number):number  => num1 * num2;
 const div = (num1:number, num2:number):number | string => (num2 !== 0) ? num1 / num2 : "Can't divide by zero"; 
-const pot = (num1:number, num2?:number):number => num1 * num1;
+const pot = (num:number):number => num * num;
+const sqrt = (num:number):number => Math.sqrt( num );
 
-const sqrt = (num1:number, num2?:number):number => Math.sqrt( num1 );
-
-function calc(callback:any, num1:number, num2?:number) {
-	typeof num1 !== 'number' ? "Only numbers are accepted." : "";
-	if(num2) {
-		typeof num2 !== 'number' ? "Only numbers are accepted." : "";
-	} 
-
-    return callback(num1, num2);
+function calc(callback:any, num1:number, num2?:number):number | string {
+	console.log(callback)
+	if( !num1 ){ return "Missed first Param" }
+	if( typeof num1 !== 'number') {  return "Only numbers are accepted." }
+	if(num2){
+		if( typeof num2 !== 'number') {  return "Only numbers are accepted." }
+		return callback(num1, num2)
+	} else { return callback(num1) }
 };
 /*
 console.log(sqrt(25));
@@ -22,7 +22,9 @@ console.log(add(25, 10));
 console.log(subt(25, 10));
 */
 //console.log(calc(add, 25, 10));
-console.log(calc(add, 25));
+//console.log(calc(add, 25));
+console.log(calc(subt, 25, 5));
+console.log(calc(subt, 25, 10));
 //console.log(calc(subt, 25, 10));
 //console.log(calc(div, 25, 10));
 //console.log(calc(sqrt, 25)); // error
