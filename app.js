a26ddb836415b67bfbcd275a56d540ca116f4348
ibernabel <id.bernabel@gmail.com>
2023-08-07 21:44:34 -0400

    refactor: Code formated and added retur new Error instead string

--- app.js
+++ app.js
@@ -1,28 +1,29 @@
 var add = function (num1, num2) { return num1 + num2; };
 var subt = function (num1, num2) { return num1 - num2; };
 var mult = function (num1, num2) { return num1 * num2; };
-var divd = function (num1, num2) { return (num2 !== 0) ? num1 / num2 : "Can't divide by zero"; };
-var pot = function (num) { return num * num; };
+var divd = function (num1, num2) { return (num2 !== 0) ? num1 / num2 : new Error("Can't divide by zero"); };
+var pot = function (num, expot) { return Math.pow(num, expot); };
+var pot2 = function (num) { return num * num; };
 var sqrt = function (num) { return Math.sqrt(num); };
 function calc(callback, num1, num2) {
     //console.log(num1);
     if (typeof num1 == "undefined" || num1 === null) {
-        return "The first parameter is missing";
+        return new Error("The first parameter is missing");
     }
     if (typeof num1 !== 'number') {
-        return "Only numbers accepted.";
+        return new Error("Only numbers accepted.");
     }
     if (!num2) {
         console.log(callback.name);
-        if (callback.name !== "sqrt" && callback.name !== "pot") {
-            return "This function required two numeric parameters, and you have passed only one";
+        if (callback.name !== "sqrt" && callback.name !== "pot2") {
+            return new Error("This function required two numeric parameters, and you have passed only one");
         }
         return callback(num1);
         //return;
     }
     else {
         if (typeof num2 !== 'number') {
-            return "Only numbers accepted";
+            return new Error("Only numbers accepted");
         }
         return callback(num1, num2);
     }
--- app.ts
+++ app.ts
@@ -1,31 +1,34 @@
-const add = (num1:number, num2:number):number => num1 + num2;
-const subt = (num1:number, num2:number):number  => num1 - num2;
-const mult = (num1:number, num2:number):number  => num1 * num2;
-const divd = (num1:number, num2:number):number | string => (num2 !== 0) ? num1 / num2 : "Can't divide by zero"; 
-const pot = (num:number):number => num * num;
-const sqrt = (num:number):number => Math.sqrt( num );
+const add = (num1: number, num2: number): number => num1 + num2;
+const subt = (num1: number, num2: number): number => num1 - num2;
+const mult = (num1: number, num2: number): number => num1 * num2;
+const divd = (num1: number, num2: number): number | Error => (num2 !== 0) ? num1 / num2 : new Error("Can't divide by zero");
+const pot = (num: number, expot: number): number => num ** expot;
+const pot2 = (num: number): number => num * num;
+const sqrt = (num: number): number => Math.sqrt(num);
 
-function calc(callback:any, num1:number, num2?:number):number | string {
+function calc(callback: any, num1: number, num2?: number): number | string | Error {
 
-    //console.log(num1);
-    if (typeof num1 == "undefined" || num1 === null) {
-        return "The first parameter is missing";
-    }
-    if (typeof num1 !== 'number') {
-        return "Only numbers accepted.";
-    }
-    if (!num2) {
+	//console.log(num1);
+	if (typeof num1 == "undefined" || num1 === null) {
+		return new Error("The first parameter is missing");
+	}
+	if (typeof num1 !== 'number') {
+		return new Error("Only numbers accepted.");
+	}
+	if (!num2) {
 		console.log(callback.name)
-		if( callback.name !== "sqrt" && callback.name !== "pot" ) {
-			return "This function required two numeric parameters, and you have passed only one";
+		if (callback.name !== "sqrt" && callback.name !== "pot2") {
+			return new Error("This function required two numeric parameters, and you have passed only one");
 		}
-        return callback(num1);
-        //return;
-    }
-    else {
-        if (typeof num2 !== 'number') { return "Only numbers accepted"; }
-        return callback(num1, num2);
-    }
+		return callback(num1);
+		//return;
+	}
+	else {
+		if (typeof num2 !== 'number') { 
+			return new Error("Only numbers accepted"); 
+		}
+		return callback(num1, num2);
+	}
 };
 
 //console.log(calc(add, 25, 10));
