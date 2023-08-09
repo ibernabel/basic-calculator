9e3624bfd289381004bc60efa9d980c1aa0cf05b
solufime <soporte.solufime@gmail.com>
2023-08-09 17:49:53 -0400

    refactor: Allowed missed parameter. Defined default params for each callback function

--- app.js
+++ app.js
@@ -1,39 +1,46 @@
-var add = function (num1, num2) { return num1 + num2; };
-var subt = function (num1, num2) { return num1 - num2; };
-var mult = function (num1, num2) { return num1 * num2; };
-var divd = function (num1, num2) { return (num2 !== 0) ? num1 / num2 : new Error("Can't divide by zero"); };
-var pot = function (num, expot) { return Math.pow(num, expot); };
-var pot2 = function (num) { return num * num; };
-var sqrt = function (num) { return Math.sqrt(num); };
+var add = function (num1, num2) {
+    if (num1 === void 0) { num1 = 0; }
+    if (num2 === void 0) { num2 = 0; }
+    return num1 + num2;
+};
+var subt = function (num1, num2) {
+    if (num1 === void 0) { num1 = 0; }
+    if (num2 === void 0) { num2 = 0; }
+    return num1 - num2;
+};
+var mult = function (num1, num2) {
+    if (num1 === void 0) { num1 = 1; }
+    if (num2 === void 0) { num2 = 1; }
+    return num1 * num2;
+};
+var divd = function (num1, num2) {
+    if (num1 === void 0) { num1 = 1; }
+    if (num2 === void 0) { num2 = 1; }
+    return (num2 !== 0) ? num1 / num2 : new Error("Can't divide by zero");
+};
+var pot = function (num, expot) {
+    if (num === void 0) { num = 1; }
+    if (expot === void 0) { expot = 2; }
+    return Math.pow(num, expot);
+};
+//const pot2 = (num: number = 1): number => num * num;
+var sqrt = function (num) {
+    if (num === void 0) { num = 1; }
+    return Math.sqrt(num);
+};
 function calc(callback, num1, num2) {
-    //console.log(num1);
-    if (typeof num1 == "undefined" || num1 === null) {
-        return new Error("The first parameter is missing");
-    }
-    if (typeof num1 !== 'number') {
+    if (typeof num1 !== 'number' && typeof num1 !== 'undefined') {
         return new Error("Only numbers accepted.");
     }
-    if (num2 === undefined || num2 === null) {
-        console.log(callback.name);
-        if (callback.name !== "sqrt" && callback.name !== "pot2") {
-            return new Error("This function required two numeric parameters, and you have passed only one");
-        }
-        return callback(num1);
-        //return;
+    if (typeof num2 !== 'number' && typeof num2 !== 'undefined') {
+        return new Error("Only numbers accepted");
     }
-    else {
-        if (typeof num2 !== 'number') {
-            return new Error("Only numbers accepted");
-        }
-        return callback(num1, num2);
-    }
+    return callback(num1, num2);
 }
 ;
-//console.log(calc(add, 25, 10));
-//console.log(calc(add, 25));
-//console.log(calc(subt, 25, 5));
-//console.log(calc(subt, 25, 10));
-//console.log(calc(subt, 25, 10));
-console.log(calc(divd, 25, 0));
-//console.log(calc(sqrt, 25));
-//console.log(calc(pot, 9));
+console.log(calc(add, 5));
+console.log(calc(subt, 8));
+console.log(calc(mult, 4));
+console.log(calc(divd, 9));
+console.log(calc(pot, 5, 3));
+console.log(calc(sqrt, 81, 3));
--- app.ts
+++ app.ts
@@ -1,41 +1,29 @@
-const add = (num1: number, num2: number): number => num1 + num2;
-const subt = (num1: number, num2: number): number => num1 - num2;
-const mult = (num1: number, num2: number): number => num1 * num2;
-const divd = (num1: number, num2: number): number | Error => (num2 !== 0) ? num1 / num2 : new Error("Can't divide by zero");
-const pot = (num: number, expot: number): number => num ** expot;
-const pot2 = (num: number): number => num * num;
-const sqrt = (num: number): number => Math.sqrt(num);
+const add = (num1: number = 0, num2: number = 0): number => num1 + num2;
+const subt = (num1: number = 0, num2: number = 0): number => num1 - num2;
+const mult = (num1: number = 1, num2: number = 1): number => num1 * num2;
+const divd = (num1: number = 1, num2: number = 1): number | Error => (num2 !== 0) ? num1 / num2 : new Error("Can't divide by zero");
+const pot = (num: number = 1, expot: number = 2): number => num ** expot;
+//const pot2 = (num: number = 1): number => num * num;
+const sqrt = (num: number = 1): number => Math.sqrt(num);
 
-function calc(callback: any, num1: number, num2?: number): number | string | Error {
+function calc(callback: any, num1?: number, num2?: number): number | string | Error {
 
-	//console.log(num1);
-	if (typeof num1 == "undefined" || num1 === null) {
-		return new Error("The first parameter is missing");
-	}
-	if (typeof num1 !== 'number') {
+	if (typeof num1 !== 'number' && typeof num1 !== 'undefined') {
 		return new Error("Only numbers accepted.");
 	}
-	if (num2 === undefined || num2 === null) {
-		console.log(callback.name)
-		if (callback.name !== "sqrt" && callback.name !== "pot2") {
-			return new Error("This function required two numeric parameters, and you have passed only one");
-		}
-		return callback(num1);
-		//return;
+
+	if (typeof num2 !== 'number' && typeof num2 !== 'undefined') { 
+		return new Error("Only numbers accepted"); 
 	}
-	else {
-		if (typeof num2 !== 'number') { 
-			return new Error("Only numbers accepted"); 
-		}
-		return callback(num1, num2);
-	}
+	
+	return callback(num1, num2);
+
 };
 
-//console.log(calc(add, 25, 10));
-//console.log(calc(add, 25));
-//console.log(calc(subt, 25, 5));
-//console.log(calc(subt, 25, 10));
-//console.log(calc(subt, 25, 10));
-console.log(calc(divd, 25, 0));
-//console.log(calc(sqrt, 25));
-//console.log(calc(pot, 9));
+
+console.log(calc(add,5));
+console.log(calc(subt, 8));
+console.log(calc(mult, 4));
+console.log(calc(divd, 9));
+console.log(calc(pot, 5, 3));
+console.log(calc(sqrt, 81, 3));
